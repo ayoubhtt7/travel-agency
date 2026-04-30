573b773479d3f1f9bf23280fd1cd8c4118659a2a
ayoubhtt7 <ayoubhtt23@gmail.com>
2026-04-30 01:59:18 +0100

    add real-time flight

--- config/services.php
+++ config/services.php
@@ -27,6 +27,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'aviationstack' => [
+      'key' => env('AVIATIONSTACK_KEY'),
+      ],
 
     'slack' => [
         'notifications' => [
